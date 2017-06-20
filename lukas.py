a3521fef762c69bbe23924345c888b48272cb608
monkeybard <papalulu27@gmail.com>
2017-06-20 15:27:54 +1000

    Update lukas.py

--- lukas.py
+++ lukas.py
@@ -10,5 +10,9 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
+@bot.command
+async def hi():
+    await bot.say("I like to lose myself in the books here when I can. It should be no surprise. Even I like a good escape.")
+    
 #token = os.environ.get('TOKEN')
 bot.run('MzI2NTgxNTcwNjkxMzk5Njgy.DCo9SQ.Ezm4TJi0wDIx1kNz7Pv91TT5bqU')
