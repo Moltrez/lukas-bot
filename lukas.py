6de850d5f01e30c8eb99ae6edaa323cacb406421
monkeybard <papalulu27@gmail.com>
2017-06-20 14:54:59 +1000

    Create lukas.py

---
+++ lukas.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+import asyncio
+import re
+import random
+import urllib
+import urllib.request
+import urllib.error
+import os
+import json
+
+bot = commands.Bot(command_prefix=['!'], description='Hello.')
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+    await bot.change_presence(game=discord.Game(name="Fire Emblem Echoes: Shadows of Valentia"))
+
+token = os.environ.get('TOKEN', default=None)
+if token is None:
+    token = open('./token').read().replace('\n','')
+bot.run(token)
