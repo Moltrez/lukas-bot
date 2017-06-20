64fd7060d81bb8b903fb2ac18f3b29c10f692a4e
monkeybard <papalulu27@gmail.com>
2017-06-20 15:42:05 +1000

    Update lukas.py

--- lukas.py
+++ lukas.py
@@ -1,16 +1,7 @@
 import discord
 from discord.ext import commands
 
-# bot = commands.Bot(command_prefix=['!'], description='Hello.')
-bot = discord.Client()
-
-@bot.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-    # echo
-    await bot.send_message(message.channel, message.content)
+bot = commands.Bot(command_prefix=['!'], description='Hello.')
 
 @bot.event
 async def on_ready():
@@ -19,10 +10,9 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
-# @bot.command
-# async def hi():
-#     quotes = ["I like to lose myself in the books here when I can. It should be no surprise. Even I like a good escape.", "I am of a noble family...at least in the world where I am from.Our home is near the border, so I joined the Deliverance when crisis erupted in our lands."]
-#     await bot.say(random.choice(quotes))
+@bot.command
+async def hi():
+    quotes = ["I like to lose myself in the books here when I can. It should be no surprise. Even I like a good escape.", "I am of a noble family...at least in the world where I am from.Our home is near the border, so I joined the Deliverance when crisis erupted in our lands."]
+    await bot.say(random.choice(quotes))
     
-# token = os.environ.get('TOKEN')
-bot.run("MzI2NTgxNTcwNjkxMzk5Njgy.DCpGcg.GxCyyiXhWVMUWLtF6uvf-3mR0RU")
+token = os.environ.get('TOKEN')
