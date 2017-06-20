fd788a7749e63bd672969a2bafea29b0a8cae85a
monkeybard <papalulu27@gmail.com>
2017-06-20 15:14:55 +1000

    Update lukas.py

--- lukas.py
+++ lukas.py
@@ -1,13 +1,5 @@
 import discord
 from discord.ext import commands
-import asyncio
-import re
-import random
-import urllib
-import urllib.request
-import urllib.error
-import os
-import json
 
 bot = commands.Bot(command_prefix=['!'], description='Hello.')
 
@@ -17,7 +9,6 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
-    await bot.change_presence(game=discord.Game(name="Fire Emblem Echoes: Shadows of Valentia"))
 
 token = os.environ.get('TOKEN', default=None)
 bot.run(token)
