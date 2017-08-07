f23b8cdb5d909e097715401e5eef2f91cb9e8760
Darren Zhu <darrenzhu27@gmail.com>
2017-08-07 23:40:43 +1000

    Parses arguments using mediawiki api so all redirects from wiki are preserved, hero and weapon icon thumbnails working. Just gotta get information and then do skills.

--- utilities.py
+++ utilities.py
@@ -2,15 +2,70 @@ import discord, asyncio, random, urllib.request, urllib3, json
 from discord.ext import commands as bot
 
 feh_source = "https://feheroes.gamepedia.com/%s"
+INVALID_HERO = 'no'
 
 
-def get_icon(arg):
-    url = feh_source % "api.php?action=query&titles=File:Icon_Portrait_%s.png&prop=imageinfo&iiprop=url&format=json" % arg
+def sanitize_url(url):
+    return url.replace(' ', '%20').replace('(', '%28').replace(')', '%29')
+
+
+def get_page(url):
+    url = sanitize_url(url)
     print(url)
     response = urllib.request.urlopen(url)
-    info = json.load(response)
-    print(info)
+    return json.load(response)
+
+
+def true_page(arg):
+    # extra cases for common aliases
+    if arg.lower() == 'frobin':
+        return 'Robin (F)'
+    if arg.lower() == 'fcorrin':
+        return 'Corrin (F)'
+    if arg.lower() == 'mrobin':
+        return 'Robin (M)'
+    if arg.lower() == 'mcorrin':
+        return 'Corrin (M)'
+    if arg.lower() == 'babe':
+        return 'Lukas'
+    if arg.lower() == 'atiki':
+        return 'Tiki (Adult)'
+    if arg.lower() == 'ytiki':
+        return 'Tiki (Young)'
+
+    # convert arg to title case, in the case of (A), (F), (BB), etc. convert stuff in parentheses to upper
+    arg = arg.split('(')
+    arg[0] = arg[0].title()
+    if len(arg) > 1:
+        if len(arg[1]) <= 3:
+            arg[1] = arg[1].upper()
+        else:
+            arg[1] = arg[1].title()
+    arg = '('.join(arg)
+
+    redirect = feh_source % "api.php?action=query&titles=%s&redirects=true&format=json" % arg
+    info = get_page(redirect)
+    if '-1' in info['query']['pages']:
+        return INVALID_HERO
+    if 'redirects' in info['query']:
+        return info['query']['redirects'][0]['to']
+    return arg
+
+
+def get_icon(arg, prefix):
+    url = feh_source % "api.php?action=query&titles=File:%s%s.png&prop=imageinfo&iiprop=url&format=json" % (prefix, arg)
+    info = get_page(url)
     return info['query']['pages'][next(iter(info['query']['pages']))]['imageinfo'][0]['url']
+
+
+def get_category(arg):
+    url = feh_source % "api.php?action=query&titles=%s&prop=categories&format=json" % arg
+    info = get_page(url)
+    categories = info['query']['pages'][next(iter(info['query']['pages']))]['categories']
+    if len(categories) > 1:
+        return categories[1]['title']
+    else:
+        return categories[0]['title']
 
 
 class Utilities:
@@ -27,14 +82,20 @@ class Utilities:
     @bot.command()
     async def feh(self, *, arg):
         """I will provide some information on a Fire Emblem Heroes topic."""
-        arg = arg.title()
-        arg = arg.replace(' ', '%20')
+        arg = true_page(arg)
+        if arg == INVALID_HERO:
+            await self.bot.say("I'm afraid I couldn't find information on that.")
+            return
+        print(arg)
+        category = get_category(arg)
         message = discord.Embed(
-            title=arg.replace('%20', ' '),
-            url=feh_source % arg,
+            title=arg,
+            url=sanitize_url(feh_source % arg)
         )
-        message.set_thumbnail(url=get_icon(arg))
-        message.add_field(name="Test", value="Testing", inline=False)
+        if category == 'Category:Heroes':
+            message.set_thumbnail(url=get_icon(arg, "Icon_Portrait_"))
+        elif category == 'Category:Weapons':
+            message.set_thumbnail(url=get_icon(arg, "Weapon_"))
         await self.bot.say(embed=message)
 
 
