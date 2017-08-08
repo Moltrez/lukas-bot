ecd94142e1ec776e6eca2ec0b211ac33630d1703
Darren Zhu <darrenzhu27@gmail.com>
2017-08-08 18:08:42 +1000

    added reactions and emotions

--- lukas_bot.py
+++ lukas_bot.py
@@ -1,4 +1,4 @@
-import discord, os
+import discord, os, re
 from discord.ext import commands
 
 import utilities
@@ -12,6 +12,19 @@ async def on_ready():
     print(bot.user.id)
     print('------')
     await bot.change_presence(game=discord.Game(name="FEHWiki"))
+
+
+@bot.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == bot.user:
+        return
+    luke_pattern = re.compile('.*gotta.*love.*luke', re.I)
+    if luke_pattern.match(message.content):
+        await bot.upload('emotions/upset.png')
+    lukas_pattern = re.compile('.*love.*lukas', re.I)
+    if lukas_pattern.match(message.content):
+        await bot.upload('emotions/happy.png')
 
 token = os.environ.get('TOKEN', default=None)
 if token is None:
