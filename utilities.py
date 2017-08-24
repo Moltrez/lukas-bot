5b8a7cbb6c31c62798e54e57c0c88678d6c6e5a7
Darren Zhu <darrenzhu27@gmail.com>
2017-08-24 14:01:37 +1000

    updated utilities to fix several skills

--- utilities.py
+++ utilities.py
@@ -1,17 +1,11 @@
-import discord, random, urllib.request, json
+import discord, random, urllib.request, urllib.parse, json
 from discord.ext import commands as bot
 from bs4 import BeautifulSoup as BSoup
 
 feh_source = "https://feheroes.gamepedia.com/%s"
 INVALID_HERO = 'no'
 
-
-def sanitize_url(url):
-    return url.replace(' ', '%20').replace('(', '%28').replace(')', '%29').replace('+', '%2B').replace("'S", '%27s')
-
-
 def get_page(url):
-    url = sanitize_url(url)
     print(url)
     response = urllib.request.urlopen(url)
     return json.load(response)
@@ -35,6 +29,8 @@ def true_page(arg):
         return 'Tiki (Young)'
     if arg.lower() == 'doot':
         return 'Delthea'
+    if arg.lower() == 'thorp':
+        return 'Tharja'
 
     # convert arg to title case, in the case of (A), (F), (BB), etc. convert stuff in parentheses to upper
     arg = arg.split('(')
@@ -45,10 +41,11 @@ def true_page(arg):
         else:
             arg[1] = arg[1].title()
     arg = '('.join(arg)
-    arg = arg.replace("'S", "'s").replace(" And ", " and ").\
-        replace('Hp', 'HP').replace('Atk ', 'Attack ').replace('Spd ', 'Speed ').replace('Def ', 'Defense ').replace('Res ', 'Resistance ')
+    arg = arg.replace("'S", "'s").replace(" And ", " and ").replace(" Or ", " or ").replace(" Of ", " of ").\
+        replace('Hp ', 'HP ').replace('Atk ', 'Attack ').replace('Spd ', 'Speed ').replace('Def ', 'Defense ').replace('Res ', 'Resistance ').replace(' +', ' Plus').\
+        replace('Hp+', 'HP Plus').replace('Atk+', 'Attack Plus').replace('Spd+', 'Speed Plus').replace('Def+', 'Defense Plus').replace('Res+', 'Resistance Plus')
 
-    redirect = feh_source % "api.php?action=query&titles=%s&redirects=true&format=json" % arg
+    redirect = feh_source % "api.php?action=query&titles=%s&redirects=true&format=json" % (urllib.parse.quote(arg))
     info = get_page(redirect)
     if '-1' in info['query']['pages']:
         return INVALID_HERO
@@ -60,7 +57,7 @@ def true_page(arg):
 def get_icon(arg, prefix=""):
     url = feh_source %\
           "api.php?action=query&titles=File:%s%s.png&prop=imageinfo&iiprop=url&format=json" %\
-          (prefix, arg.replace('+', '_Plus' + '_' if not prefix == "Weapon_" else ''))
+          (prefix, urllib.parse.quote(arg.replace('+', '_Plus' + '_' if not prefix == "Weapon_" else '')))
     info = get_page(url)
     if '-1' in info['query']['pages']:
         return None
@@ -68,14 +65,14 @@ def get_icon(arg, prefix=""):
 
 
 def get_categories(arg):
-    url = feh_source % "api.php?action=query&titles=%s&prop=categories&format=json" % arg
+    url = feh_source % "api.php?action=query&titles=%s&prop=categories&format=json" % (urllib.parse.quote(arg))
     info = get_page(url)
     categories = info['query']['pages'][next(iter(info['query']['pages']))]['categories']
     return [a['title'].lstrip('Category:') for a in categories]
 
 
 def get_text(arg):
-    url = feh_source % "api.php?action=parse&page=%s&format=json" % arg
+    url = feh_source % "api.php?action=parse&page=%s&format=json" % (urllib.parse.quote(arg))
     info = get_page(url)
     return info['parse']['text']['*']
 
@@ -115,7 +112,7 @@ def calc_bst(stats_table):
     for key in stats_table[-1]:
         if key == 'Rarity':
             continue
-        if '-' in stats_table[-1][key]:
+        if '-' in stats_table[-1][key] or '?' in stats_table[-1][key]:
             return None
         stat_arr = stats_table[-1][key].split('/')
         bst += int(stat_arr[1 if len(stat_arr) > 1 else 0])
@@ -143,7 +140,7 @@ class Utilities:
         print(arg)
         message = discord.Embed(
             title=arg,
-            url=sanitize_url(feh_source % arg),
+            url=feh_source % (urllib.parse.quote(arg)),
             color=0xe74c3c
         )
         categories = get_categories(arg)
@@ -155,9 +152,11 @@ class Utilities:
             stats = get_infobox(html)
             base_stats_table, max_stats_table = [extract_table(a)
                                                  for a in html.find_all("table", attrs={"class":"wikitable"})[1:3]]
+            print(stats)
+            rarity = ', '.join(a+'★' for a in stats['Rarities'] if a.isdigit())
             message.add_field(
                 name="Rarities",
-                value=', '.join(a+'★' for a in stats['Rarities'] if a.isdigit()),
+                value= rarity if rarity else 'N/A'
             )
             message.add_field(
                 name="BST",
