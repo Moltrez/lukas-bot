4fb12845dd93ddeac81c95d04043b001cc71c8a4
Darren Zhu <darrenzhu27@gmail.com>
2017-10-18 11:01:30 +1100

    Changed command prefix to ?

--- lukas_bot.py
+++ lukas_bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 
 import utilities
 
-bot = commands.Bot(command_prefix=['!', 'lukas ', 'Lukas '], description='I am here to serve. I will try to respond to messages that start with `!` or `lukas `.')
+bot = commands.Bot(command_prefix=['?', 'lukas ', 'Lukas '], description='I am here to serve. I will try to respond to messages that start with `!` or `lukas `.')
 
 @bot.event
 async def on_ready():
