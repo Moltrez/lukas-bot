1f10ae62ad3f9a5646b4930ec5de415aabf13497
monkeybard <papalulu27@gmail.com>
2017-10-24 22:21:48 +1100

    all flaunts are uniform size

--- feh_alias.py
+++ feh_alias.py
@@ -97,17 +97,17 @@ waifus = {
 }
 
 flaunt = {
-    'InvdrZim13#0826':'https://cdn.discordapp.com/attachments/369523314126749706/370016114102763520/Screenshot_20171017-211149.png',
-    'MCProductions#6839':'https://cdn.discordapp.com/attachments/369523314126749706/371841366264774657/Screenshot_20171022-210353.png',
-    'monkeybard#3663':'https://cdn.discordapp.com/attachments/369522639955296268/372265366665297920/Screenshot_20171024-170814.png',
-    'Blaze#1888':'https://cdn.discordapp.com/attachments/367370320962650113/370263882058563594/Heroes_Roy.png',
-    'ErrantDShepherd#8839':'https://cdn.discordapp.com/attachments/367370320962650113/370023047652048896/Screenshot_20171017-183902.png',
-    'bookofholsety#2235':'https://cdn.discordapp.com/attachments/369523314126749706/370042437411930112/2017-10-18_13.54.58.png',
-    'Dr. Letha#0629':'https://cdn.discordapp.com/attachments/369523314126749706/370042170209468426/Screenshot_20171010-093438.png',
-    'Datagne#6415':'https://cdn.discordapp.com/attachments/369523314126749706/370107115513315329/Screenshot_20171018-081321-058.png',
-    'Mr.Pepsi™#8225':'https://cdn.discordapp.com/attachments/369522639955296268/372315394461007878/Screenshot_2017-10-24-17-27-19.png',
-    'Zylphe#7994':'https://cdn.discordapp.com/attachments/369522639955296268/370154665700098058/Screenshot_2017-10-15-11-54-43-61.png',
-    'Zmr56 is Bored#0800':'https://cdn.discordapp.com/attachments/369522639955296268/370362796400967701/Screenshot_2017-09-16-17-28-35.png',
-    'Chastlily/Lys#8151':'https://cdn.discordapp.com/attachments/370949018488012800/371259186689998848/image-5.png',
-    'East#2679':'https://cdn.discordapp.com/attachments/370949018488012800/371259318517104640/image-6.png'
+    'InvdrZim13#0826':'https://media.discordapp.net/attachments/369523314126749706/370016114102763520/Screenshot_20171017-211149.png',
+    'MCProductions#6839':'https://media.discordapp.net/attachments/369523314126749706/371841366264774657/Screenshot_20171022-210353.png',
+    'monkeybard#3663':'https://media.discordapp.net/attachments/369522639955296268/372265366665297920/Screenshot_20171024-170814.png',
+    'Blaze#1888':'https://media.discordapp.net/attachments/367370320962650113/370263882058563594/Heroes_Roy.png',
+    'ErrantDShepherd#8839':'https://media.discordapp.net/attachments/367370320962650113/370023047652048896/Screenshot_20171017-183902.png',
+    'bookofholsety#2235':'https://media.discordapp.net/attachments/369523314126749706/370042437411930112/2017-10-18_13.54.58.png',
+    'Dr. Letha#0629':'https://media.discordapp.net/attachments/369523314126749706/370042170209468426/Screenshot_20171010-093438.png',
+    'Datagne#6415':'https://media.discordapp.net/attachments/369523314126749706/370107115513315329/Screenshot_20171018-081321-058.png',
+    'Mr.Pepsi™#8225':'https://media.discordapp.net/attachments/369522639955296268/372315394461007878/Screenshot_2017-10-24-17-27-19.png',
+    'Zylphe#7994':'https://media.discordapp.net/attachments/369522639955296268/370154665700098058/Screenshot_2017-10-15-11-54-43-61.png',
+    'Zmr56 is Bored#0800':'https://media.discordapp.net/attachments/369522639955296268/370362796400967701/Screenshot_2017-09-16-17-28-35.png',
+    'Chastlily/Lys#8151':'https://media.discordapp.net/attachments/370949018488012800/371259186689998848/image-5.png',
+    'East#2679':'https://media.discordapp.net/attachments/370949018488012800/371259318517104640/image-6.png'
 }
