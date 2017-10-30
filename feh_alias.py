cbe2b579a6a216b7dbf5302e7697ce22fe86aa98
Darren <darrenzhu27@gmail.com>
2017-10-31 09:29:45 +1100

    update mc

--- feh_alias.py
+++ feh_alias.py
@@ -106,7 +106,7 @@ flaunt = {
     'Karinole#6528':'https://media.discordapp.net/attachments/369522639955296268/372488306908200960/Screenshot_20171024-165500.png',
     'Darrman#2798':'https://media.discordapp.net/attachments/369522639955296268/372487390637457409/Screenshot_2017-10-24-21-46-541.png',
     'InvdrZim13#0826':'https://media.discordapp.net/attachments/369523314126749706/370016114102763520/Screenshot_20171017-211149.png',
-    'MCProductions#6839':'https://media.discordapp.net/attachments/369523314126749706/371841366264774657/Screenshot_20171022-210353.png',
+    'MCProductions#6839':'https://media.discordapp.net/attachments/369523314126749706/374650882639396864/Screenshot_20171030-150802.png',
     'monkeybard#3663':'https://media.discordapp.net/attachments/369522639955296268/372265366665297920/Screenshot_20171024-170814.png',
     'Blaze#1888':'https://media.discordapp.net/attachments/367370320962650113/373497728505741312/Heroes_Roy.png',
     'ErrantDShepherd#8839':'https://media.discordapp.net/attachments/367370320962650113/373499088857202688/Screenshot_20171025-184714.png',
