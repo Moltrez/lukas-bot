4dc144a05a2de8bb9dd6ace7fc0b82aa2b0ed6c6
Darren <darrenzhu27@gmail.com>
2017-12-05 12:03:32 +1100

    new file

---
+++ full_update.py
@@ -0,0 +1,29 @@
+from fehwiki_parse import *
+from feh_cache import *
+
+
+def update_category(cache, category):
+    cache.update()
+    members = get_page('http://feheroes.gamepedia.com/api.php?action=query&list=categorymembers&cmtitle=Category:%s&cmlimit=300' % category)['query']['categorymembers']
+    members = [m['title'] for m in members]
+    count = 0
+    if members:
+        for member in members:
+            if not member.startswith('Category:') and not any([m in cache.data for m in [member, member+' 1', member+' 2', member+' 3']]):
+                print("Getting data for " + member)
+                categories, data = get_data(member, 3, cache, False)
+                print("Adding alias from " + member.lower() + " to " + member)
+                cache.add_alias(member.lower(), member, False)
+                count += 1
+    print("Added " + str(count) + " " + category + " to cache")
+    if count:
+        cache.save()
+
+if __name__ == '__main__':
+    cache = FehCache()
+    update_category(cache, 'Heroes')
+    update_category(cache, 'Passives')
+    update_category(cache, 'Weapons')
+    update_category(cache, 'Specials')
+    update_category(cache, 'Assists')
+    print(cache.last_update)
