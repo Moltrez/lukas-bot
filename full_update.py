7c8122409554477bffa49618f09130b41c7bc6f6
Darren <darrenzhu27@gmail.com>
2017-12-05 12:17:47 +1100

    Time out behaviour for updating

--- full_update.py
+++ full_update.py
@@ -1,23 +1,27 @@
 from fehwiki_parse import *
 from feh_cache import *
-
+from socket import timeout
 
 def update_category(cache, category):
     cache.update()
     members = get_page('http://feheroes.gamepedia.com/api.php?action=query&list=categorymembers&cmtitle=Category:%s&cmlimit=300' % category)['query']['categorymembers']
     members = [m['title'] for m in members]
     count = 0
-    if members:
-        for member in members:
-            if not member.startswith('Category:') and not any([m in cache.data for m in [member, member+' 1', member+' 2', member+' 3']]):
-                print("Getting data for " + member)
-                categories, data = get_data(member, 3, cache, False)
-                print("Adding alias from " + member.lower() + " to " + member)
-                cache.add_alias(member.lower(), member, False)
-                count += 1
-    print("Added " + str(count) + " " + category + " to cache")
-    if count:
-        cache.save()
+    try:
+        if members:
+            for member in members:
+                if not member.startswith('Category:') and not any([m in cache.data for m in [member, member+' 1', member+' 2', member+' 3']]):
+                    print("Getting data for " + member)
+                    categories, data = get_data(member, 3, cache, False)
+                    print("Adding alias from " + member.lower() + " to " + member)
+                    cache.add_alias(member.lower(), member, False)
+                    count += 1
+    except timeout:
+        print("Timed out")
+    finally:
+        print("Added " + str(count) + " " + category + " to cache")
+        if count:
+            cache.save()
 
 if __name__ == '__main__':
     cache = FehCache()
