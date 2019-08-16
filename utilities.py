ab7a2f1d86854482a3064f55022d5cbe51852ae8
Darren Zhu <darrenzhu27@gmail.com>
2019-08-16 22:19:00 +1000

    update stats to accept enemy units

--- utilities.py
+++ utilities.py
@@ -273,7 +273,7 @@ class FireEmblemHeroes:
         if not unit:
             return False, data
         should_save = self.cache.add_data(args, data, categories, save=False)
-        if 'Heroes' not in categories:
+        if all([c not in categories for c in ['Heroes', 'Enemy units']]):
             if 'Disambiguation pages' in categories:
                 return should_save, ('%s could refer to:```' + data['1Could refer to:'][0] + '```Please specify the %s that you want.') % (unit, unit)
             return should_save, '%s does not seem to be a hero.' % (unit)
